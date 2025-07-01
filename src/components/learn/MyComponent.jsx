4a1dc6112790ae3acf30a80d87732e0e0ef69d32
AnAnh2k <anducanh125@gmail.com>
2025-07-01 10:23:32 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import "./style.css";
 
 export default function MyComponent() {
+  const name = "My Component";
+  const name1 = 20;
+  const name2 = null;
+  const name3 = undefined;
+  const name4 = true;
+  const name5 = [1, 2, 3];
+  const name6 = { n: "ducanh", age: 21 };
   return (
     <>
-      <div>MyComponent</div>
+      <div>{JSON.stringify(name6)}</div>
       <div className="child" style={{ fontSize: "10px" }}>
         hẹ hẹ
       </div>
