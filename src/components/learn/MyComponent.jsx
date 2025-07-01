9ea5cab744a58e1f5689b3f3a3d0bad1b59e4db8
AnAnh2k <anducanh125@gmail.com>
2025-07-01 10:11:25 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import "./style.css";
 
 export default function MyComponent() {
-  return <div>MyComponent</div>;
+  return (
+    <>
+      <div>MyComponent</div>
+      <div className="child" style={{ fontSize: "10px" }}>
+        hẹ hẹ
+      </div>
+    </>
+  );
 }
