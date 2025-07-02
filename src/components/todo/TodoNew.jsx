5ea92a440e644fd36ee4771c5f24bcb3804ba816
AnAnh2k <anducanh125@gmail.com>
2025-07-02 15:27:42 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -2,10 +2,24 @@ import React from "react";
 
 export default function TodoNew(props) {
   const { addNewTodo } = props;
+  const handleClick = () => {
+    alert("click me");
+  };
+
+  const handleOnChange = (name) => {
+    console.log("handleOnChange", name);
+  };
   return (
     <div className="todo-new">
-      <input type="text" />
-      <button onClick={addNewTodo}>Add</button>
+      <input
+        type="text"
+        onChange={(event) => {
+          handleOnChange(event.target.value);
+        }}
+      />
+      <button style={{ cursor: "pointer" }} onClick={handleClick}>
+        Add
+      </button>
     </div>
   );
 }
